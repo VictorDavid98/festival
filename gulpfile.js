7a55cd204dde919dc91f06dcf7e70aa0ff3cc82e
Victor <victorzamorac98@gmail.com>
2022-01-12 14:31:21 -0300

    añadiendo mixins

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
-const {src, dest, watch} = require('gulp');
+const {src, dest, watch, parallel} = require('gulp');
+//css
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+//Imagenes
+const cache = require('gulp-cache')
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
+
 
 function css(done){
     //Identificar el archivo .scss a compilar
@@ -11,10 +19,40 @@ function css(done){
     
     done();
 }
+function imagenes(done){
+    const opciones = {
+        optimizationLevel:3
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest('build/img'))
+    done();
+}
+function versionWebp(done){
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'))
+    done()
+}
+function versionAvif(done){
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'))
+    done()
+}
 
 function dev(done ){
     watch('src/scss/**/*.scss', css);
     done();
 }
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionWebp, versionAvif,  dev);
